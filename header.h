15bbe81a839a6421569cd052f90efd7671399cf5
FanTasy CriT <60607192+HarounTrabelsi@users.noreply.github.com>
2022-02-14 11:10:51 +0100

    Delete header.h

--- header.h
+++
@@ -1,7 +0,0 @@
-#ifndef HEADER_H_   /* Include guard */
-#define HEADER_H_
-#include <SDL/SDL_image.h>
-#include <SDL/SDL.h>/* All SDL App’s need this */
-#include <stdio.h>
-void display_bmp(SDL_Surface* screen,SDL_Rect pos);
-#endif // HEADER_H_
