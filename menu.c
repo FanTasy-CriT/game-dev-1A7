016d93819113ebab5a9f8f09e9079a40eb05fc2f
FanTasy CriT <60607192+HarounTrabelsi@users.noreply.github.com>
2022-02-15 18:15:11 +0100

    Update menu.c

--- menu.c
+++ menu.c
@@ -6,6 +6,10 @@
 
 void display_bmp(SDL_Surface* screen,SDL_Rect pos)
 {
+pos.x=0;
+pos.y=0;
+pos.h=0;
+pos.w=0;
 SDL_Surface *image;
 //Load the BMP file into a surface 
 image = SDL_LoadBMP("final.bmp");
@@ -19,7 +23,19 @@ fprintf(stderr, "BlitSurface error: %s\n", SDL_GetError());
 }
 SDL_Flip(screen);
 }
-
-void initialize_audio(){
-
+void display_img(SDL_Surface* screen,SDL_Rect pos,const char * path){
+{
+SDL_Surface *image;
+//Load the BMP file into a surface 
+image = IMG_Load(path);
+if (image == NULL) {
+fprintf(stderr, "Couldnt load : %s\n", SDL_GetError());
+return;
+}
+//Blit onto the screen surface 
+if(SDL_BlitSurface(image, NULL, screen, &pos)){
+fprintf(stderr, "BlitSurface error: %s\n", SDL_GetError());
+}
+SDL_Flip(screen);
+}
 }
